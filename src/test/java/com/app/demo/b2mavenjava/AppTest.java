ca1735c450d7dc512192b22d9a1b54b4a19d615d
p-bhavika <bhavikapoosa@gmail.com>
2024-01-05 01:12:27 +0530

    mavenjava

---
+++ src/test/java/com/app/demo/b2mavenjava/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.demo.b2mavenjava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
